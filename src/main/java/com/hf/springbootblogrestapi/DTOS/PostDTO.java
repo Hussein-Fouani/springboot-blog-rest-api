fc3d0883a0a2f673f50ee2234fa3447c0ba0dc93
Hussein <social.101.acc@gmail.com>
2023-10-03 16:14:40 +0300

    PostDTO creation

---
+++ src/main/java/com/hf/springbootblogrestapi/DTOS/PostDTO.java
@@ -0,0 +1,12 @@
+package com.hf.springbootblogrestapi.DTOS;
+
+import lombok.Data;
+
+@Data
+public class PostDTO {
+
+    private Long Id;
+    private String Title;
+    private String description ;
+    private String content;
+}
