d498b3c1590cceb9e663fca850d28834cb78f3e0
Hussein <social.101.acc@gmail.com>
2023-10-06 09:26:33 +0300

    instantiated model mapper as a Bean

--- src/main/java/com/hf/springbootblogrestapi/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/hf/springbootblogrestapi/SpringbootBlogRestApiApplication.java
@@ -1,13 +1,19 @@
 package com.hf.springbootblogrestapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootBlogRestApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
+    }
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 
 }
