1f0ff6f1f71bcbbbc47b6555ceeaa7e15873e116
Hussein <social.101.acc@gmail.com>
2023-10-06 18:03:52 +0300

    added valid annotation for parameter validation

--- src/main/java/com/hf/springbootblogrestapi/DTOS/CommentDTO.java
+++ src/main/java/com/hf/springbootblogrestapi/DTOS/CommentDTO.java
@@ -1,6 +1,9 @@
 package com.hf.springbootblogrestapi.DTOS;
 
 import com.hf.springbootblogrestapi.entity.Post;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +11,17 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CommentDTO { private long id;
+public class CommentDTO {
+    private long id;
+    @NotEmpty
+    @Size(max = 255,min = 5 ,message = "Name should be between 5 and 255 characters")
     private String name;
+    @NotEmpty
+    @Size(max = 255,min = 10 ,message = "Body should be between 10 and 255 characters")
     private String body;
+    @NotEmpty
+    @Email
+    @Size(max = 255,message = "Email shouldn't exceed 255 characters")
     private String email;
     private Post post;
 
--- src/main/java/com/hf/springbootblogrestapi/controller/CommentController.java
+++ src/main/java/com/hf/springbootblogrestapi/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.hf.springbootblogrestapi.controller;
 
 import com.hf.springbootblogrestapi.DTOS.CommentDTO;
  import com.hf.springbootblogrestapi.service.CommentService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +18,7 @@ public class CommentController {
     private CommentService commentService;
 
     @PostMapping("posts/{postId}/comment")
-    public ResponseEntity<CommentDTO> createComment(@PathVariable long postId,@RequestBody  CommentDTO commentDTO){
+    public ResponseEntity<CommentDTO> createComment(@PathVariable long postId,@Valid @RequestBody  CommentDTO commentDTO){
         return new ResponseEntity<>(commentService.createComment(commentDTO,postId), HttpStatus.CREATED);
     }
 
@@ -32,7 +33,7 @@ public class CommentController {
     }
 
     @PutMapping("posts/{postId}/comment/{commentId}")
-    public ResponseEntity<CommentDTO> updateComment(@PathVariable Long postId,@PathVariable Long commentId,@RequestBody CommentDTO commentDTO){
+    public ResponseEntity<CommentDTO> updateComment(@PathVariable Long postId,@PathVariable Long commentId,@Valid @RequestBody CommentDTO commentDTO){
         CommentDTO commentDTO1 =commentService.updateComment(postId,commentId,commentDTO);
         return new ResponseEntity<>(commentDTO1,HttpStatus.OK);
     }
