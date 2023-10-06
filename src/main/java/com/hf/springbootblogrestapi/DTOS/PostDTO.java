31de12822073910edda7897f4007e9a0baaf78b1
Hussein <social.101.acc@gmail.com>
2023-10-06 09:28:12 +0300

    added set of comments for representation

--- src/main/java/com/hf/springbootblogrestapi/DTOS/PostDTO.java
+++ src/main/java/com/hf/springbootblogrestapi/DTOS/PostDTO.java
@@ -2,10 +2,13 @@ package com.hf.springbootblogrestapi.DTOS;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class PostDTO {
     private Long Id;
     private String Title;
     private String description ;
     private String content;
+    private Set<CommentDTO> comments;
 }
