e969a7e1f8d4941baeffded6571972480db4fa36
Hussein <social.101.acc@gmail.com>
2023-10-06 18:04:50 +0300

    extending the class ResponseEntityExceptionHandler for custom error message return

--- src/main/java/com/hf/springbootblogrestapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hf/springbootblogrestapi/exception/GlobalExceptionHandler.java
@@ -1,18 +1,23 @@
 package com.hf.springbootblogrestapi.exception;
 
 import com.hf.springbootblogrestapi.DTOS.ErrorDetails;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler  {
-
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest){
         ErrorDetails errorDetails = new ErrorDetails(new Date(),exception.getMessage(), webRequest.getDescription(false));
@@ -25,5 +30,13 @@ public class GlobalExceptionHandler  {
         ErrorDetails errorDetails = new ErrorDetails(new Date(),exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails,HttpStatus.BAD_REQUEST);
     }
-
-}
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String ,String> errorDetails = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error)->{
+            String fieldName = ((FieldError)error).getField();
+            String message = error.getDefaultMessage();
+            errorDetails.put(fieldName,message);
+        });
+        return new ResponseEntity<>(errorDetails,HttpStatus.BAD_REQUEST);
+    }
+    }
